1fc28a19d9e51c8e7d81575137761093da3be993
Helmi Agustian <helmi.agustian@bukalapak.com>
2026-05-16 20:26:29 +0700

    nurul-ilmi basepath

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   output: 'export',
   // Ganti 'nurul-ilmi' dengan nama repo GitHub Anda
   // Hapus basePath jika pakai custom domain
-  // basePath: '/nurul-ilmi',
+  basePath: '/nurul-ilmi',
   images: {
     unoptimized: true,
   },
